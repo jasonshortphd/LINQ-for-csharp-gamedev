e35475b49b88d4abd7f5c391dabb95c5325e9bad
Jason Short <jasonshortphd@users.noreply.github.com>
2017-08-27 19:56:12 -0700

    Boxing library
    I have had notes on this type of game for a long time

---
+++ boxing/FightGameLogic/FIghterBuild.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FightGameLogic
+{
+    public class FighterBuild
+    {
+        public enum BuildTypes
+        {
+            Barrel = 15,
+            Stocky = 10,
+            Broiad = 5,
+            Medium = 0,
+            Lean = -5,
+            Lanky = -10,
+            Skinny = -15
+        };
+
+
+    }
+}
